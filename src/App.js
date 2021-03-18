3db40f3ad5c43eecac88920915e41b609154ccd5
Tom Guibard <tom.guibardpro@gmail.com>
2021-03-18 15:36:43 +0100

    trop fort

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Beth Cook" avatar="https://randomuser.me/api/portraits/women/66.jpg" isOnline={true}/>
-      <Contact name="Daniel Simmons" avatar="https://randomuser.me/api/portraits/men/88.jpg" isOnline={false}/>
-      <Contact name="Dale Prescott" avatar="https://randomuser.me/api/portraits/men/14.jpg" isOnline={true}/>
+      <ContactList/>
     </div>
   );
 }
